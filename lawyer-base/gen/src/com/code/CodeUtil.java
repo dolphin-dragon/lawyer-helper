9b7c17ca7357035cafc2ebd10c064df96da8e150
lizhiyuan <lizhiyuan@lizhiyuan-PC.aspire.aspire-tech.com>
2020-04-12 11:38:20 +0800

    修订代码生成配置处理

--- lawyer-base/gen/src/com/code/CodeUtil.java
+++ lawyer-base/gen/src/com/code/CodeUtil.java
@@ -8,8 +8,8 @@ public class CodeUtil {
 		String tables[] = new String[] {"case_apply","case_carry_out","case_customer","case_first_instance","case_info","case_pre_litigation","case_second_instance","sys_user_ext"};
 		String tableInfo[] = new String[]{"案件确立阶段信息","案件执行阶段信息","客户信息","案件一审阶段信息","案件基础信息","案件诉前和解信息","案件二审阶段信息","用户信息设置"};
 		
-		//tables = new String[] { "sys_user_ext"};
-		//tableInfo = new String[]{"用户设置"};
+		tables = new String[] { "case_m_info"};
+		tableInfo = new String[]{"案件综合信息"};
 		
 		for (int i = 0; i < tables.length; i++) {
 			String tableName = tables[i];
